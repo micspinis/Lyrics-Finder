0ffe371d6170363b06a1097e657cbcc319a6a207
Miguel Somohano <miguel.spinis@gmail.com>
2020-07-06 10:20:56 -0500

    Agregando Busqueda y validacion al Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+
+    // state de busqueda
+    const [ busqueda, guardarBusqueda ] = useState({
+        artista: '',
+        cancion: ''
+    });
+
+    // state para validacion
+    const [ error, guardarError ] = useState(false);
+
+    // Extraccion
+    const { artista, cancion } = busqueda
+
+    // Funcion a cada input para leer su contenido
+    const actualizarState = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    // Consultar las APIs
+    const buscarInformacion = e => {
+        e.preventDefault();
+        return;
+
+        // Validacion
+        if(artista.trim()==='' || cancion.trim()==='') {
+
+        }
+    }
+    
+
     return ( 
         <div className="bg-info">
             <div className="container">
                 <div className="row">
                     <form
+                        onSubmit={buscarInformacion}
                         className="col card text-white bg-transparent mb-5 pt-5 pb-2"
                     >
                         <fieldset>
@@ -19,6 +53,8 @@ const Formulario = () => {
                                             className="form-control"
                                             name="artista"
                                             placeholder="Nombre Artista"
+                                            onChange={actualizarState}
+                                            value={artista}
                                         />
                                     </div>
                                 </div>
@@ -30,6 +66,8 @@ const Formulario = () => {
                                                 className="form-control"
                                                 name="cancion"
                                                 placeholder="Nombre Canción"
+                                                onChange={actualizarState}
+                                                value={cancion}
                                             />
                                     </div>
                                 </div>
